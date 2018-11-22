60b16e3e448e37b6dcdfb7c0e8170d453f9d0fcf
tsokota <y.tsokota@student.csn.khai.edu>
2018-11-22 16:21:22 +0200

    Add project files.

---
+++ ToDoPr/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToDoPr.Models;
+
+namespace ToDoPr.Controllers
+{
+    public class HomeController : Controller
+    {
+        TaskContext db = new TaskContext();
+
+        public ActionResult Index()
+        {
+            return View(db.Tasks);
+        }
+
+       
+        public ActionResult Delete(int id)
+        {
+            Task userprofile = db.Tasks.Find(id);
+            if (userprofile != null)
+            {
+                db.Tasks.Remove(userprofile);
+                db.SaveChanges();
+            }
+            return PartialView(db.Tasks);
+        }
+
+
+        public ActionResult Delete1(int id)
+        {
+            Task userprofile = db.Tasks.Find(id);
+            if (userprofile != null)
+            {
+                db.Tasks.Remove(userprofile);
+                db.SaveChanges();
+            }
+            return Redirect("/Home/Index");
+        }
+
+    }
+}
